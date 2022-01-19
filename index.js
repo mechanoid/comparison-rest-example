880230c8f98625dd5778e9966bf83a6ec80eb3d8
Falk Hoppe <falkhoppe81@gmail.com>
2022-01-19 10:21:30 +0100

    remove comment

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
     const method = req.body._method
     delete req.body._method
     return method
